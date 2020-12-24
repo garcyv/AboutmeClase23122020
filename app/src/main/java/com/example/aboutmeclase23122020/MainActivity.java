4d06ac4513a451a72b48c3adf8524df2d8580252
garcyv <garcyv@gmail.com>
2020-12-24 17:25:31 -0300

    Segundo comiit actualiza main.java añade fragment y content .java

--- app/src/main/java/com/example/aboutmeclase23122020/MainActivity.java
+++ app/src/main/java/com/example/aboutmeclase23122020/MainActivity.java
@@ -1,8 +1,18 @@
-package com.example.aboutmeclase23122020;
+package com.example.aboutme;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
-import android.os.Bundle;
+import com.example.aboutmeclase23122020.R;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        CollapsingToolbarLayout toolBarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
+        toolBarLayout.setTitle(getTitle());
+
+        ImageView imageView = findViewById(R.id.imageView3);
+
+        //"https://aboutme-images-1.s3.amazonaws.com/background/users/g/a/r/garcy_1528144909_974.jpg"
+        //"https://unsplash.com/photos/nBqVK1jF9OE/download?force=true&w=640"
+        // https://photos.google.com/photo/AF1QipPv9DEDQSsTDJz_npkcmB-Red0n7rMn7jRw3Gas
+        //     Glide.with(this).load("https://aboutme-images-1.s3.amazonaws.com/background/users/g/a/r/garcy_1528144909_974.jpg").into(imageView);
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mintent = new Intent(MainActivity.this, com.example.aboutme.ContactActivity.class);
+                startActivity(mintent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
